026d9dffe384b402524afae2a45113f61520fbc6
KOU <dryadg79585@gmail.com>
2021-10-18 23:15:16 +0900

    test add header, sass-loader

---
+++ src/components/Header.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="header">this is header<span class="header_red">red!</span></div>
+</template>
+
+<script>
+export default {
+  name: 'Header'
+}
+</script>
+
+<style scoped lang="scss">
+.header{
+    color: #fff;
+    background-color: #222;
+    &_red{
+        color: red;
+    }
+}
+</style>
